0c75e53057b68b448ffb9fe6f883eaf617a99771
karupanerura <karupa@cpan.org>
2015-09-24 20:35:56 +0900

    added test cases

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,11 +6,21 @@ class Tests: XCTestCase {
     func testInitWithLocation() {
         let geohex : GeoHex3 = GeoHex3(lat: 35.622866, lng: 139.725552, level: 7)
         XCTAssertEqual(geohex.code, "XM4885168")
+        XCTAssertEqual(geohex.x, 11254)
+        XCTAssertEqual(geohex.y, -4025)
+        XCTAssertEqual(geohex.lat, 35.6227556354106)
+        XCTAssertEqual(geohex.lng, 139.725651577503)
+        XCTAssertEqual(geohex.level, 7)
     }
 
     func testInitWithXY() {
         let geohex : GeoHex3 = GeoHex3(x: 11254, y: -4025, level: 7)
         XCTAssertEqual(geohex.code, "XM4885168")
+        XCTAssertEqual(geohex.x, 11254)
+        XCTAssertEqual(geohex.y, -4025)
+        XCTAssertEqual(geohex.lat, 35.6227556354106)
+        XCTAssertEqual(geohex.lng, 139.725651577503)
+        XCTAssertEqual(geohex.level, 7)
     }
 
     func testInitWithCode() {
@@ -18,6 +28,8 @@ class Tests: XCTestCase {
         XCTAssertEqual(geohex.code, "XM4885168")
         XCTAssertEqual(geohex.x, 11254)
         XCTAssertEqual(geohex.y, -4025)
+        XCTAssertEqual(geohex.lat, 35.6227556354106)
+        XCTAssertEqual(geohex.lng, 139.725651577503)
         XCTAssertEqual(geohex.level, 7)
     }
 
